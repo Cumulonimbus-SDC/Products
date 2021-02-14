f15fd0c294ddf066b0c6ba8bbffdf648622d2f0e
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-02-14 14:30:40 -0500

    Include Redis, Add Redis to docker-compose

--- createIndexes.sql
+++ createIndexes.sql
@@ -1,11 +1,11 @@
--- CREATE INDEX idx_products
---   ON products(product_id);
+CREATE INDEX idx_products
+  ON products(product_id);
 
--- CREATE INDEX idx_features
---   ON features(product_id);
+CREATE INDEX idx_features
+  ON features(product_id);
 
--- CREATE INDEX idx_styles
---   ON styles(product_id);
+CREATE INDEX idx_styles
+  ON styles(product_id);
 
 CREATE INDEX idx_skus
   ON skus(style_id);
